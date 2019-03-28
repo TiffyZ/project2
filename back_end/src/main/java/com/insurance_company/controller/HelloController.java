a435b562eab416c2cf0b57f905a420d2e28410eb
张张靖远 <jingyuan.zhang@ucdconnect.ie>
2019-03-28 19:13:15 +0800

    19/3/28 尝试申请赔款功能1“

--- back_end/src/main/java/com/insurance_company/controller/HelloController.java
+++ back_end/src/main/java/com/insurance_company/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.insurance_company.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ResponseBody
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
-public class HelloController {
-    @RequestMapping
-    public String hello(){
-        return "hello world";
+public class HelloController{
+    @ResponseBody
+    @RequestMapping("/hello")
+    public  String hello(){
+        return "Hello";
+    }
+    @RequestMapping({"/","/index.html"})
+    public String index(){
+        return "index";
     }
 }
+
