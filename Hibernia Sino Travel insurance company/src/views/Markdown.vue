bc5ddb81146c063ef4927f74a63b01d71a7adcfd
16372212 <869106524@qq.com>
2019-03-29 20:47:08 +0800

    changed appli-table

--- Hibernia Sino Travel insurance company/src/views/Markdown.vue
+++ Hibernia Sino Travel insurance company/src/views/Markdown.vue
@@ -1,30 +1,42 @@
 <template>
+<div  :style="backgroundDiv">
   <div class="components-container">
 
 <h3 class="intro-head">用户申请窗口👍</h3>
 
 <center>
+
+
 <div class="input_control">
 <table>
-<caption style="text-decoration:underline;font-weight:bold;">Application Table</caption>
-<tr ><td>Name：</td><td><input width="100%" type="text"></td></tr>
-<tr><td>Phone：</td><td><input type="text"></td></tr>
-<tr><td>E-mail：</td><td><input type="text"></td></tr>
-<tr><td>Gender：</td><td><select><option>male</option><option>female</option></select></td></tr>
-<tr><td>Submit Options：</td><td><select><option>a</option><option>b</option><option>c</option><option>d</option></select></td></tr>
-<tr><td>Remark：</td><td><textarea></textarea></td></tr>
+
+<caption style="text-decoration:underline;font-weight:bold;"><font face="verdana" color="orange" size="5">APPLICATION TABLE</font></caption>
+<tr ><td>Name：</td><td><Input width="100%" type="text"></Input></td></tr>
+<tr><td>Phone：</td><td><Input type="tel"></Input></td></tr>
+<tr><td>E-mail：</td><td><Input type="email"></Input></td></tr>
+<tr><td>Gender：</td><td><Radio-group>
+                <Radio label="male">male</Radio>
+                <Radio label="female">female</Radio>
+              </Radio-group></td></tr>
+<tr><td>Submit Options：</td><td><Select><Option>a</Option><Option>b</Option><Option>c</Option><Option>d</Option></Select></td></tr>
+<tr><td>Remark：</td><td><Input type="tel"></Input></td></tr>
 </table>
-<Button type="input" shape="circle">Submit</Button>
-<Button type="reset" shape="circle">Recite</Button>
+    <Button type="info">SUBMIT</Button>
+    <Button type="warning">RESET</Button>
+
+
+<!-- <input type="submit" value="submit"/>
+<input type="reset" value="reset" />
+ -->
 <!-- <input type="submit" color = white value="submit" >　<input type="reset" color=black value="Recite"> -->
 
 
 </div>
 
 </center> 
-
 <mavon-editor  style="  margin-top: 80px" v-model="content"/>
-  </div>
+</div>
+</div>
 </template>
 
 
@@ -33,20 +45,23 @@
 
 
 
-table,table tr th, table tr td { border:1px solid #0094ff; }
+table,table tr th, table tr td { border:1px solid #C0C0C0; }
 table { width: 50%; min-height: 25px; line-height: 25px; text-align: center; border-collapse: collapse;}   
 .input_control{
   width: 100%;
-  margin:20px auto;
+  margin:120px auto;
+
 }
+
+
 input[type="text"].#btn1,#btn2{
   box-sizing: border-box;
   text-align:center;
   font-size:1.4em;
-  height:2.7em;
+  height:3.7em;
   border-radius:4px;
-  border:1px solid #c8cccf;
-  color:#6a6f77;
+  border:3px solid #c8cccf;
+  color:"white";
   -web-kit-appearance:none;
   -moz-appearance: none;
   display:block;
@@ -60,4 +75,17 @@ input[type="text"]:focus{
 }
 </style>
 
+<script type="text/javascript">
+  export default {
+  data() {
+    return {
+    backgroundDiv: {
+        backgroundImage:'url(' + require('static/img/blue2.png') + ')',
+        height:"100%",
 
+        backgroundRepeat:'no-repeat',backgroundSize:'100% 250%'    
+        }
+      }
+  }
+}
+</script>
