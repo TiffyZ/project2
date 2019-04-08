c2de3ec0b247d5ed5d5531eb875321423bd511ed
张张靖远 <jingyuan.zhang@ucdconnect.ie>
2019-04-08 22:12:50 +0800

    back end

---
+++ interest-server/src/main/java/com/interest/controller/template/MsgRecordsController.java
@@ -0,0 +1,34 @@
+package com.interest.controller.template;
+
+import com.interest.model.entity.PageResult;
+import com.interest.model.utils.ResponseWrapper;
+import com.interest.service.MsgRecordsService;
+import com.interest.utils.SecurityAuthenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MsgRecordsController {
+
+    @Autowired
+    private MsgRecordsService msgRecordsService;
+
+
+    @GetMapping("/msgrecords/unreadnum")
+    public ResponseWrapper<Integer> getUnreadMsgCount() {
+        return new ResponseWrapper<>(msgRecordsService.getUnreadMsgCount(SecurityAuthenUtil.getId()));
+    }
+
+    @GetMapping("/msgrecords/user")
+    public ResponseWrapper<PageResult> userMesRecordsGet(@RequestParam("pageSize") int pageSize, @RequestParam("page") int page) {
+        PageResult pageResult = msgRecordsService.getUserMegsResult(pageSize, page * pageSize);
+        return new ResponseWrapper<>(pageResult);
+    }
+
+    @PutMapping("/msgrecords/read")
+    public ResponseWrapper<Integer> userReadMes(@RequestParam("msgRecordId") Integer msgRecordId) {
+        msgRecordsService.updateMsgRecordIsread(msgRecordId, 1);
+        return new ResponseWrapper<Integer>(msgRecordId);
+    }
+
+}
