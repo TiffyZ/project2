d6b94cc48eba36b6af134da998dadd8564322ef7
wenqi997 <1125377852@qq.com>
2019-04-13 22:32:16 +0800

    用户表单页初期修改，未设置只能查看自身版本

--- Hibernia-sino_Travel_Insurance_Company/src/views/article/pc/home.vue
+++ Hibernia-sino_Travel_Insurance_Company/src/views/article/pc/home.vue
@@ -1,128 +1,229 @@
-<style scoped>
-  .card-body {
-    padding: 10px;
-  }
-  .content{
-    word-wrap: break-word;
-    word-break: break-all;
-    overflow: hidden;
-  }
-  .chat{
-    display: inline;
-    margin-right: 50px;
-    color:#f90;
-  }
-  .chat span{
-    margin-left: 2px;
-  }
-  .title {
-    margin-bottom: 8px;
-  }
-  .title span{
-    color: #2d64b3;
-  }
-  .info {
-    margin-bottom: 5px;
-  }
-  .info p{
-    width:100%;
-    overflow:hidden;
-    text-overflow:ellipsis; 
-    white-space:nowrap;
-  }
-  .list-user-other {
-    height: 24px;
-    line-height: 24px;
-  }
-  .list-user-other dt{
-    float: left;
-    margin: 0 6px 0 0;
-  }
-  .list-user-other dd{
-    float: left;
-    font-size: 14px;
-    color: #8a8a8a;
-    line-height: 24px;
-  }
-  .list-user-other .right-info {
-    line-height: 24px;
-    float: right;
-  }
-  .list-user-other .right-info .text {
-    margin-right: 4px;
-    color: #8a8a8a;
-  }
-  .list-user-other .right-info .num {
-    color: #3399ea;
-  }
-  .interval {
-    float: left;
-    width: 1px;
-    height: 12px;
-    border-radius: 50%;
-    background-color: #e0e0e0;
-    margin-top: 6px;
-    margin-left: 8px;
-    margin-right: 8px;
-  }
-</style>
-<template>
 
+<template>
+    <div style="margin: 20px;">
+        <div>
+            <ul>
+                <li>
+                    <Button type="error" icon="md-trash" @click="del()">删除</Button>
+                </li>
+                <li>
+                    <div style="padding: 10px 0;">
+                        <Table border :columns="columns1" :data="data1" :height="400" @on-selection-change="s=>{change(s)}" @on-row-dblclick="s=>{dblclick(s)}"></Table>
+                    </div>
+                </li>
+                <li>
+                    <div style="text-align: right;">
+                        <Page :total="total" :page-size="pageInfo.pageSize" show-elevator show-total @on-change="e=>{pageSearch(e)}"></Page>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <Modal :mask-closable="false" :visible.sync="modal" v-model="modal" width="600" title="查看">
+            <Form :label-width="80" >
+                <Form-item label="用户名:">
+                    <strong>{{email.username}}</strong>
+                    <!-- <Input v-model="email.username" style="width: 204px" disabled="disabled" /> -->
+                </Form-item>
+                <Form-item label="内容:">
+                    <span>{{email.content}}</span>
+                    <!-- <Input v-model="email.username" style="width: 204px" disabled="disabled" /> -->
+                </Form-item>
+            </Form>
+            <div slot="footer">
+                <Button type="error" size="large"  @click="cancel">关闭</Button>
+            </div>
+        </Modal>
+    </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      searchContent:null,
-      articleList: [],
-      total: 0,
-      pageInfo: {
-        page: 0,
-        pageSize: 20
-      }
-    };
-  },
-  mounted() {
-    this.getArticle({
-      pageInfo: this.pageInfo
-    });
-  },
-  watch: {
-    $route: ["search"]
-  },
-  methods: {
-    search(){
-      this.searchContent = this.$route.query.searchValue;
-      this.getArticle({
-        pageInfo: this.pageInfo
-      });
+    data() {
+        return {
+            /*修改modal的显示参数*/
+            modal: false,
+            /*分页total属性绑定值*/
+            total: 0,
+            /*pageInfo实体*/
+            pageInfo: {
+                page: 0,
+                pageSize: 10
+            },
+            /*user实体*/
+            email: {
+                id: null,
+                username: null,
+                title: null,
+                email: null,
+                name: null,
+                content: null,
+                createtime: null
+            },
+            /*表显示字段*/
+            columns1: [
+                {
+                    type: "selection",
+                    width: 60,
+                    align: "center"
+                },
+                {
+                    title: "姓名",
+                    key: "name"
+                },
+                {
+                    title: "标题",
+                    width: 500,
+                    key: "title"
+                },
+                {
+                    title: "email",
+                    key: "email"
+                },
+                {
+                    title: "时间",
+                    key: "createtime"
+                },
+                {
+                    title: "操作",
+                    align: "center",
+                    key: "action",
+                    render: (h, params) => {
+                        return h("div", [
+                            h(
+                                "Button",
+                                {
+                                    props: {
+                                        type: "info"
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.emailInfo(params.row);
+                                        }
+                                    }
+                                },
+                                "查看"
+                            )
+                        ]);
+                    }
+                }
+            ],
+            /*表数据*/
+            data1: []
+        };
     },
-    getArticle(e) {
-      this.axios({
-        method: "get",
-        url: "/public/articles",
-        params: {
-          page: e.pageInfo.page,
-          pageSize: e.pageInfo.pageSize,
-          searchContent: this.searchContent
-        }
-      })
-        .then(
-          function(response) {
-            this.articleList = response.data.data.data;
-            this.total = response.data.data.totalCount;
-          }.bind(this)
-        )
-        .catch(function(error) {
-          alert(error);
+    mounted() {
+        /*页面初始化调用方法*/
+        this.getTable({
+            pageInfo: this.pageInfo
         });
     },
-    pageSearch(e) {
-      this.pageInfo.page = e - 1;
-      this.getArticle({
-        pageInfo: this.pageInfo,
-      });
+    methods: {
+        /*pageInfo实体初始化*/
+        initPageInfo() {
+            this.pageInfo.page = 0;
+            this.pageInfo.pageSize = 10;
+        },
+        emailSet(e) {
+            this.email.id = e.id;
+            this.email.username = e.username;
+            this.email.title = e.title;
+            this.email.email = e.email;
+            this.email.name = e.name;
+            this.email.content = e.content;
+            this.email.createtime = e.createtime;
+        },
+        dateGet(e) {
+            var time = new Date(parseInt(e));
+            return (
+                time.getFullYear() +
+                "-" +
+                (time.getMonth() + 1) +
+                "-" +
+                time.getDate() +
+                " " +
+                time.getHours() +
+                ":" +
+                time.getMinutes()
+            );
+        },
+        listDateSet(e) {
+            for (var i = e.length - 1; i >= 0; i--) {
+                e[i].createtime = this.dateGet(e[i].createtime);
+            }
+        },
+        /*得到表数据*/
+        getTable(e) {
+            this.axios({
+                method: "get",
+                url: "/emails",
+                params: {
+                    page: e.pageInfo.page,
+                    pageSize: e.pageInfo.pageSize
+                }
+            })
+                .then(
+                    function(response) {
+                        this.data1 = response.data.data.data;
+                        this.listDateSet(this.data1);
+                        this.total = response.data.data.totalCount;
+                    }.bind(this)
+                )
+                .catch(function(error) {
+                    alert(error);
+                });
+        },
+        /*分页点击事件*/
+        pageSearch(e) {
+            this.pageInfo.page = e - 1;
+            this.getTable({
+                pageInfo: this.pageInfo
+            });
+        },
+        /*modal的cancel点击事件*/
+        cancel() {
+            this.modal = false;
+        },
+        /*表格中双击事件*/
+        dblclick(e) {
+            this.emailSet(e);
+            this.modal = true;
+        },
+        emailInfo(e) {
+            this.emailSet(e);
+            this.modal = true;
+        },
+        del() {
+            if (this.groupId != null && this.groupId != "") {
+                this.axios({
+                    method: "delete",
+                    url: "/admin/emails",
+                    data: this.groupId
+                })
+                    .then(
+                        function(response) {
+                            this.getTable({
+                                pageInfo: this.pageInfo
+                            });
+                            this.groupId = [];
+                            this.$Message.info("删除成功");
+                        }.bind(this)
+                    )
+                    .catch(function(error) {
+                        alert(error);
+                    });
+            }
+        },
+        change(e) {
+            this.setGroupId(e);
+        },
+        setGroupId(e) {
+            this.groupId = [];
+            for (var i = 0; i <= e.length - 1; i++) {
+                this.groupId.push(e[i].id);
+            }
+        }
     }
-  }
 };
+
+
+
 </script>
