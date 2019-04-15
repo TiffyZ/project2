dd07677c9902b967cadd74a5e9e7828fb90a87c2
张张靖远 <jingyuan.zhang@ucdconnect.ie>
2019-04-15 11:33:12 +0800

    重新上传后端

---
+++ interest-server/src/main/java/com/interest/controller/template/PostCardController.java
@@ -0,0 +1,51 @@
+package com.interest.controller.template;
+
+import java.util.List;
+
+import com.interest.model.entity.PageResult;
+import com.interest.model.entity.PostCardEntity;
+import com.interest.model.utils.ResponseWrapper;
+import com.interest.model.response.PostCardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.interest.service.PostCardService;
+
+@RestController
+public class PostCardController {
+
+    @Autowired
+    private PostCardService postCardService;
+
+    @GetMapping("/public/postcards")
+    public ResponseWrapper<PageResult> postcardList(@RequestParam(value = "interestid", required = false) String interestid,
+                                                    @RequestParam("pageSize") int pageSize, @RequestParam("page") int page) {
+        PageResult pageResult = new PageResult();
+        pageResult.setData(postCardService.postcardList(interestid, pageSize, page * pageSize));
+        pageResult.setTotalCount(postCardService.postcardSize(interestid, pageSize, page * pageSize));
+        return new ResponseWrapper<>(pageResult);
+    }
+
+    @GetMapping("/public/postcards/postcard")
+    public ResponseWrapper<PostCardResponse> postcardGet(@RequestParam("id") int id) {
+        return new ResponseWrapper<>(postCardService.getPostcard(id));
+    }
+
+    @PostMapping("/postcards/postcard")
+    public ResponseWrapper<PostCardEntity> insertEntity(@RequestBody PostCardEntity postCardEntity) {
+        postCardService.insertEntity(postCardEntity);
+        return new ResponseWrapper<>(postCardEntity);
+    }
+
+    @DeleteMapping("/postcards")
+    public ResponseWrapper<List<String>> deletePostcards(@RequestBody List<String> groupId) {
+        postCardService.deletePostcards(groupId);
+        return new ResponseWrapper<>(groupId);
+    }
+
+}
---
+++ interest-server/src/main/java/com/interest/model/entity/EmailEntity.java
@@ -0,0 +1,119 @@
+package com.interest.model.entity;
+
+/**
+ * @author wanghuan
+ *
+ */
+public class EmailEntity {
+	private Integer id;
+	
+	private String title;
+	
+	private String email;
+	
+	private String name;
+	
+	private String content;
+	
+	private String createtime;
+
+	private Integer userid;
+
+	private String formType;
+
+	private String image;
+
+	private Integer label;
+
+	private String remark;
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Integer getLabel() {
+		return label;
+	}
+
+	public void setLabel(Integer label) {
+		this.label = label;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getFormType() {
+		return formType;
+	}
+
+	public void setFormType(String formType) {
+		this.formType = formType;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(String createtime) {
+		this.createtime = createtime;
+	}
+
+	public Integer getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+}
